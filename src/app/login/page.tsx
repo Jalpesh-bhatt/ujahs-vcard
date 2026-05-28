9634cbc5e5934e339a80958ef52eff741fae2e9e
Jalpesh Bhatt <jalpesh.bhatt@ujahs.com>
2026-05-28 15:17:06 +0530

    fixed login await issue

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,18 @@
-await signIn("credentials")
+"use client";
+
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+
+  async function handleLogin() {
+    await signIn("credentials");
+  }
+
+  return (
+    <div>
+      <button onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+}
